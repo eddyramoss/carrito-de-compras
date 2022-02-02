f9d5d5a2fa765e9d4cddbda9064d2b04da4110b0
Eddy Ramos <eddyinf605@gmail.com>
2022-02-02 18:11:33 -0400

    se agrego la funcion para obtener los datos en objetos

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -14,8 +14,25 @@ function cargarEventListeners(){
 // funciones
 function agregarCurso (e){
     e.preventDefault();
+
+
     if (e.target.classList.contains('agregar-carrito')){
-        console.log('Agregando al carrito ');
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
     }
     
+}
+
+// lee el contendo de html al que le dumos click y extrae la informacion del curso
+function leerDatosCurso(curso){
+console.log(curso);        
+    // crear un objeto conel contenido del curso 
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    console.log(infoCurso);
 }
