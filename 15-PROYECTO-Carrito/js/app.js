aea459b47a3d1faa951f8ec5c789d9e4c41c3e50
Eddy Ramos <eddyinf605@gmail.com>
2022-02-02 17:46:28 -0400

    se agrego las variables y la funcion de precionar

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -3,4 +3,19 @@
 const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarrito = document.querySelector('#vaciar-carrito');
-const listaCursos = document.querySelector('#lista.cursos');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners(){
+    //cuando agregas a un curso presionando "agregar al carrito"
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+// funciones
+function agregarCurso (e){
+    e.preventDefault();
+    if (e.target.classList.contains('agregar-carrito')){
+        console.log('Agregando al carrito ');
+    }
+    
+}
